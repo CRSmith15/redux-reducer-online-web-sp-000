d9d602140ee3ca9acbd7a44a29c0af7325a30190
Chance Smith <chancesmith97@hotmail.com>
2020-10-06 04:31:37 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,7 @@
 export function managePresents(state, action){
   switch(action.type) {
     case 'INCREASE':
-      return {count: stat.count + 1}
+      return {count: state.count + 1}
     default:
       return state;
   }
